9991756d4aef534d1e323a15dc44c6f165f8a54f
Dheeraj-Khakre <khakredheeraj@gmail.com>
2025-09-24 18:07:45 +0530

    added ai chat memory  using mysql

--- backend/src/main/java/com/portfolio/ai_config/AIConfig.java
+++ backend/src/main/java/com/portfolio/ai_config/AIConfig.java
@@ -3,9 +3,11 @@ package com.portfolio.ai_config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +18,7 @@ public class AIConfig {
     public ChatMemory getChatMemory(JdbcChatMemoryRepository repository){
         return MessageWindowChatMemory.builder()
                 .chatMemoryRepository(repository)
-                .maxMessages(10)
+                .maxMessages(20)
                 .build();
     }
 
@@ -25,6 +27,12 @@ public class AIConfig {
         MessageChatMemoryAdvisor messageChatMemoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
 
         return ChatClient.builder(cerebrasChatModel)
+                .defaultAdvisors(messageChatMemoryAdvisor, SimpleLoggerAdvisor.builder().build())
+                .defaultOptions(ChatOptions.builder()
+                        .maxTokens(700)
+                        .model("gpt-oss-120b")
+                        .temperature(0.4)
+                        .build())
                 .build();
     }
 }
